3ea4ec8d8c730548d64844c3bfaffb129415fa1e
damonyiu <dmy6@sfu.ca>
2025-03-24 23:24:51 -0700

    creating joke.js and main.js

--- .github/actions/joke-action/joke.js
+++ .github/actions/joke-action/joke.js
@@ -15,5 +15,22 @@ async function getJoke() {
   return res.joke;
 }
 
+module.exports = getJoke;const request = require("request-promise");
+
+const options = {
+  method: "GET",
+  uri: "https://icanhazdadjoke.com/",
+  headers: {
+    Accept: "application/json",
+    "User-Agent": "Writing JavaScript action GitHub Skills course.",
+  },
+  json: true,
+};
+
+async function getJoke() {
+  const res = await request(options);
+  return res.joke;
+}
+
 module.exports = getJoke;
 
